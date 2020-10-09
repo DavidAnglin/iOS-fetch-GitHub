512c362b4275f3c3398101b5a08cf7b61919646c
David Anglin <DAAnglin6389@gmail.com>
2020-10-09 09:17:10 -0500

    Initial Commit

---
+++ iOS-fetch-GitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-fetch-GitHub
+//
+//  Created by David Anglin on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
